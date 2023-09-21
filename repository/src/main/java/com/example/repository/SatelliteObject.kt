07166fe3237136566dea86b9e2a058f6a8c9347e
akaratakan <akaratakan34@gmail.com>
2023-09-22 02:01:48 +0300

    Missed files for last commit

--- repository/src/main/java/com/example/repository/SatelliteObject.kt
+++
@@ -1,7 +0,0 @@
-package com.example.repository
-
-object SatelliteObject {
-    const val SATELLITE_LIST = "satellite_list.json"
-    const val SATELLITE_DETAIL_LIST = "satellite_detail_list.json"
-    const val SATELLITE_POSITION_LIST = "satellite_position_list.json"
-}
